c666416e92d631ddf24a602445ccee273dc570ab
Valentina <gvalya.25@gmail.com>
2021-01-17 12:50:35 +0300

    Initial commit

---
+++ src/main/java/lesson1/MainApp.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class MainApp {
+    //1.Создать пустой проект в IntelliJ IDEA и прописать метод main().
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        //2.Создать переменные всех пройденных типов данных и инициализировать их значения.
+
+    }
+}
