12fa26e86008417b398c626f3d13f87445a4b66c
ValentinaGolovkina <gvalya.25@gmail.com>
2021-03-29 10:49:50 +0300

    added MFU

---
+++ src/main/java/lvl3_lesson4_multithreading/Main.java
@@ -0,0 +1,14 @@
+package lvl3_lesson4_multithreading;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+       // task1.print();
+
+
+        Task2 task2 = new Task2();
+        task2.run();
+    }
+
+}
