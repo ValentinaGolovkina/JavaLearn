aa98f39f0ef1c1012ff6cd67db2cf41ff4037632
ValentinaGolovkina <gvalya.25@gmail.com>
2021-03-31 10:25:25 +0300

    synchronization implemented

---
+++ src/main/java/lvl3_lesson5_multithreading_races/Race.java
@@ -0,0 +1,14 @@
+package lvl3_lesson5_multithreading_races;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Race {
+    private ArrayList<Stage> stages;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+    }
+}
+
